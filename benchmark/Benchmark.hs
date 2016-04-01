525cc6322d89868c9b775b90685688c64cfac82f
Tomas Carnecky <tomas.carnecky@gmail.com>
2016-04-02 00:24:45 +0200

    Run benchmarks against CSS files from facebook and github
    (cherry picked from commit c6b8fe982fe4a1e83d86db76687a051dd3badb71)

---
+++ benchmark/Benchmark.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Control.Monad
+import           Criterion.Main
+import           System.Directory
+import qualified Data.Text.IO as T
+import           Data.CSS.Syntax.Tokens
+
+
+
+main :: IO ()
+main = do
+    cwd <- getCurrentDirectory
+    tokenizeBenchmarks <- do
+        websites <- drop 2 <$> getDirectoryContents (cwd ++ "/benchmark/fixtures")
+        forM websites $ \website -> do
+            files <- drop 2 <$> getDirectoryContents (cwd ++ "/benchmark/fixtures/" ++ website)
+            benchmarks <- forM files $ \file -> do
+                body <- T.readFile $ cwd ++ "/benchmark/fixtures/" ++ website ++ "/" ++ file
+                pure $ bench file $ whnf tokenize body
+
+            pure $ bgroup website benchmarks
+
+    defaultMain
+        [ bgroup "tokenize" tokenizeBenchmarks
+        ]
